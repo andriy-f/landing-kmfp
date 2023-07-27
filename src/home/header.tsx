5f6612d938a6c527a799ff0c6f42046a615babe4
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2023-07-27 23:40:08 +0300

    add image

--- src/home/header.tsx
+++ src/home/header.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import { StaticImage } from 'gatsby-plugin-image'
+
 export default function Header() {
     return (
     <header>
       <h1>Live Kirtan Meidation for peace</h1>
+      <StaticImage alt="Indie band" src="../images/love-lg-2.jpg" />
     </header>
   )
 }
